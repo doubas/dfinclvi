85514e29ac6a7640e248002bdaa75cfa8346a35e
doubas <bdouaidy@gmail.com>
2025-07-28 17:03:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -122,8 +122,7 @@ for color, group in sorted(color_groups, key=lambda x: x[0]):
             st.subheader(f"Color {color}")
             st.dataframe(
                 group[[ 'Qty (Total)' , 'Item Description']],
-                use_container_width=False,
-                use_page_widyh=True,
+                use_container_width=True,
                 hide_index=True,
                 height=400
             )
